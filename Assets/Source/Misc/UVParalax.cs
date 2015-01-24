78849b55db90a6ed53c3041a11a5fe6757ff7fbd
Laminatovej Demon <plysovejkaktus@plysovejs-MacBook-Pro.local>
2015-01-24 00:50:21 +0000

    uv paralax + additive font

---
+++ Assets/Source/Misc/UVParalax.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UVParalax : MonoBehaviour 
+{
+	public float _ParalaxSpeed = 1.0f;
+	float _ParalaxValue = 0.0f;
+	
+	void Start()
+	{
+		renderer.material.mainTextureScale = new Vector2(Camera.main.aspect, 1.0f);
+	}
+	
+	void Update()
+	{
+		_ParalaxValue += _ParalaxSpeed * Time.deltaTime;
+		_ParalaxValue -= (int)_ParalaxValue;
+		renderer.material.mainTextureOffset = new Vector2(_ParalaxValue, 0.0f);
+	}
+}
