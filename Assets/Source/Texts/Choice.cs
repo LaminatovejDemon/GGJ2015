0b8a6a2c08c011bb468e6dfed87962c1d2ca7235
Laminatovej Demon <plysovejkaktus@plysovejs-MacBook-Pro.local>
2015-01-24 04:44:32 +0000

    choice animations

--- Assets/Source/Texts/Choice.cs
+++ Assets/Source/Texts/Choice.cs
@@ -6,6 +6,7 @@ public class Choice : MonoBehaviour
 	void OnEnable() 
 	{
 		InputController.RegisterListener(OnInput);	
+		animation.PlayQueued("ChoiceIdle");
 	}
 	
 	void OnInput(TouchPhase phase, Collider targetObject, Vector3 position)
@@ -16,5 +17,13 @@ public class Choice : MonoBehaviour
 		}
 		
 		Debug.Log (gameObject.name + ": " + phase + " to " + position);
+	}
+
+	public void SetText(string label)
+	{
+		for ( int i = 0; i < transform.childCount; ++i )
+		{
+			transform.GetChild(i).GetComponent<TextMesh>().text = label;
+		}
 	}
 }
--- Assets/Source/Texts/TextManager.cs
+++ Assets/Source/Texts/TextManager.cs
@@ -45,34 +45,45 @@ public class TextManager : MonoBehaviour
 
 	public void OnSentenceFinished(Sentence source)
 	{
-		DisplayChoices(source._NextSentenceList);
+		StartCoroutine(DisplayChoices(source._NextSentenceList));
 	}
 
-	void DisplayChoices(List<Sentence.NextSentence> choices)
+	void DisplayChoice(Sentence.NextSentence item, List<Transform> placesList)
 	{
+		Choice choiceInstance_ = GameObject.Instantiate(_ChoiceTemplate) as Choice;
+		choiceInstance_.transform.position = Vector3.zero;
+		choiceInstance_.SetText(item._Target.name);
+		
+		choiceInstance_.transform.parent = GetChoicePosition(placesList);
+		choiceInstance_.transform.localPosition = Vector3.zero;
+		
+		choiceInstance_.gameObject.AddComponent<BoxCollider>();
+		choiceInstance_.GetComponent<BoxCollider>().center = Vector3.zero;
+		Vector3 bounds_ = choiceInstance_.transform.GetChild(0).renderer.bounds.extents * 2.0f;
+		bounds_.z = 1.0f;
+		choiceInstance_.GetComponent<BoxCollider>().size = bounds_;
+	}
+
+	IEnumerator DisplayChoices(List<Sentence.NextSentence> choices)
+	{
+	
 		List<Transform> _choicesList = new List<Transform>(_ChoicePositions);
 
 		for ( int i = 0; i < Mathf.Min(3, choices.Count); ++i )
 		{
-			Choice choiceInstance_ = GameObject.Instantiate(_ChoiceTemplate) as Choice;
-			choiceInstance_.transform.position = Vector3.zero;
-			choiceInstance_.name = choiceInstance_.GetComponent<TextMesh>().text = choices[i]._Target.name;
-
-			choiceInstance_.transform.parent = GetChoicePosition(_choicesList);
-			choiceInstance_.transform.localPosition = Vector3.zero;
-
-			choiceInstance_.gameObject.AddComponent<BoxCollider>();
-			choiceInstance_.GetComponent<BoxCollider>().center = Vector3.zero;
-			Vector3 bounds_ = choiceInstance_.renderer.bounds.extents * 2.0f;
-			bounds_.z = 1.0f;
-			choiceInstance_.GetComponent<BoxCollider>().size = bounds_;
+			DisplayChoice(choices[i], _choicesList);
+			yield return new WaitForSeconds(0.5f / ((float)i+1));
 		}
+
+		yield break;
 	}
 
 	Transform GetChoicePosition(List<Transform> choicesList)
 	{
-		int index_ = Random.Range(0, choicesList.Count);
-		Transform ret_ = choicesList[index_];
+		int index_ = Random.Range(0, choicesList.Count / 2) * 2;
+
+		Transform ret_ = choicesList[index_ + Random.Range(0,2) ];
+		choicesList.RemoveAt(index_);
 		choicesList.RemoveAt(index_);
 
 		return ret_;
